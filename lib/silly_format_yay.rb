116e931efd95d6e2e1e2818a0bee99a8fd606bcf
Macey Baker <maceybaker@Maceys-MacBook-Pro-3.local>
2016-06-08 16:55:40 +0100

    updated output

--- lib/silly_format_yay.rb
+++ lib/silly_format_yay.rb
@@ -4,15 +4,15 @@ require 'colorize'
 class SillyFormatYay
   # This registers the notifications this formatter supports, and tells
   # us that this was written against the RSpec 3.x formatter API.
-  RSpec::Core::Formatters.register self, *[:example_passed, :example_failed]
+  RSpec::Core::Formatters.register self, *[:example_started, :example_passed, :example_failed]
 
     def initialize(output)
       @output = output
     end
 
-#   def example_started(notification)
-#     @output << "example: " << notification.example.description
-#   end
+  def example_started(notification)
+    @output << "example: " << notification.example.description
+  end
 # end
     def example_passed(example)
       # if MacOS.version >= :lion
